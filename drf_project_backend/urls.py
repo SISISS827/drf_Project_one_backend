371791166e32db0ee6843f95126c7206b5f1b722
taehee_Jung <palzmnbvcx@gmail.com>
2022-11-18 10:32:44 +0900

    feat : 게시글, 댓글 생성 수정 삭제  기능 추가

--- drf_project_backend/urls.py
+++ drf_project_backend/urls.py
@@ -2,12 +2,18 @@ from allauth.account.views import confirm_email
 from django.urls import re_path
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
+    path('articles/', include('articles.urls')),
     re_path(r'^auth/', include('rest_auth.urls')),
     re_path(r'^auth/signup/', include('rest_auth.registration.urls')),
     re_path(r'^account/', include('allauth.urls')),
     re_path(r'^accounts-rest/registration/account-confirm-email/(?P<key>.+)/$', confirm_email, name='account_confirm_email'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
